e75c9d082b4ea6a1d4b17f5c3d1859d347f09267
Matthew Cavallo <matt.cavallo@outlook.com>
2020-04-15 14:20:40 -0500

    Startup added - installed Microsoft.Azure.Functions.Extensions

---
+++ extensionsConfigFuncTest5601_V2/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+[assembly: FunctionsStartup(typeof(extensionsConfigFuncTest5601_V2.Startup))]
+
+namespace extensionsConfigFuncTest5601_V2
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+
+            var config = configBuilder.Build();
+        }
+    }
+}
